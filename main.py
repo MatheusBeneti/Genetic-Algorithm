a1502e2ad7a9ac68b12075c6cb541915ccf3927f
MatheusBeneti <matheusbeneti3@gmail.com>
2023-07-14 23:44:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#----------------------------------------------------------------
+# primeiro criar a estrutura para representar as pessoas ou cidades 
+# A lista ira conter as cidade e a sigla do aeroporto
+destinationCity = 'Roma'
+flightsDataPath = 'flights.txt'
+people = ['Paris','Berlin']
+flights = []
+
+def readDataFlights(flightsDataPath):
+    for i in open(flightsDataPath):
+        print(i)
+
+# 1,1,2,3
+def fitness(flights):
+    return 0
+    
+
+
+readDataFlights(flightsDataPath)
